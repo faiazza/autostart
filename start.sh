fe985369e14ef1c13a011e539b18d2866e33edd8
faiazza <paul@faiazza.com>
2023-10-30 01:55:56 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget cron
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget cron vim curl
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
@@ -31,24 +31,9 @@ echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
 wget ${GITHUB_DOWNLOAD_URL} -P ~/ccminer
 if [ -f ~/ccminer/config.json ]
 then
-  INPUT=
-  while [ "$INPUT" != "y" ] && [ "$INPUT" != "n" ]
-  do
-    printf '"~/ccminer/config.json" already exists. Do you want to overwrite? (y/n) '
-    read INPUT
-    if [ "$INPUT" = "y" ]
-    then
-      echo "\noverwriting current \"~/ccminer/config.json\"\n"
-      rm ~/ccminer/config.json
-    elif [ "$INPUT" = "n" ]
-    then
-      echo "saving as \"~/ccminer/config.json.#\""
-    else
-      echo 'Invalid input. Please answer with "y" or "n".\n'
-    fi
-  done
+rm ~/ccminer/config.json
 fi
-wget https://raw.githubusercontent.com/faiazza/Android-Mining/main/config.json -P ~/ccminer
+wget https://raw.githubusercontent.com/faiazza/autostart/main/config.json -P ~/ccminer
 
 if [ -f ~/ccminer/ccminer ]
 then
@@ -57,32 +42,4 @@ fi
 mv ~/ccminer/${GITHUB_DOWNLOAD_NAME} ~/ccminer/ccminer
 chmod +x ~/ccminer/ccminer
 
-cat << EOF > ~/ccminer/start.sh
-#!/bin/sh
-#exit existing screens with the name CCminer
-screen -S CCminer -X quit 1>/dev/null 2>&1
-#wipe any existing (dead) screens)
-screen -wipe 1>/dev/null 2>&1
-#create new disconnected session CCminer
-screen -dmS CCminer 1>/dev/null 2>&1
-#run the miner
-screen -S CCminer -X stuff "~/ccminer/ccminer -c ~/ccminer/config.json\n" 1>/dev/null 2>&1
-printf '\nMining started.\n'
-printf '===============\n'
-printf '\nManual:\n'
-printf 'start: ~/.ccminer/start.sh\n'
-printf 'stop: screen -X -S CCminer quit\n'
-printf '\nmonitor mining: screen -x CCminer\n'
-printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
-EOF
-chmod +x start.sh
 
-echo "setup nearly complete."
-echo "Edit the config with \"nano ~/ccminer/config.json\""
-
-echo "go to line 15 and change your worker name"
-echo "use \"<CTRL>-x\" to exit and respond with"
-echo "\"y\" on the question to save and \"enter\""
-echo "on the name"
-
-echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
